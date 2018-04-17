ccd67c9c9d462bf2aa7bd42729b07df513ff186a
zongtb <zongtb@126.com>
2018-04-17 14:01:57 -0700

    layout finish

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,12 +5,23 @@ import {Profile} from "./Profile";
 
 export class Main extends React.Component {
     state = {
-        playerId : nba.findPlayer('Stephen Curry').playerId
+        playerId : nba.findPlayer('Stephen Curry').playerId,
+        playerInfo: {},
+    }
+
+    componentDidMount() {
+        nba.stats.playerInfo( {PlayerID : this.state.playerId} ).then((info) => {
+            console.log(info);
+            const playInfo = Object.assign(info.commonPlayerInfo[0], info.playerHeadlineStats[0]); // object merge
+            console.log(playInfo);
+
+            this.setState({playerInfo: playInfo});
+        })
     }
     render() {
         return (
             <div className='main'>
-                <Profile />
+                <Profile playerInfo={this.state.playerInfo}/>
                 <ShotChart playerId={this.state.playerId}/>
             </div>
         );
---
+++ src/constants.js
@@ -0,0 +1,5 @@
+// 用来抠图
+// hard code的东西都放在这里
+export const PROFILE_PIC_URL_PREFIX = 'https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/260x190'; // 用来抠图
+
+export const TEAM_PIC_URL_PREFIX = 'https://stats.nba.com/media/img/teams/logos'; // 用来抠图
